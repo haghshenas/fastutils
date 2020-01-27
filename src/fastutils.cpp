c1c02bbcaba0d54d62a1b23cd55a0e67e654a4fd
Ehsan Haghshenas <ehaghshe@sfu.ca>
2020-01-27 14:36:39 -0800

    print version if requested

--- src/fastutils.cpp
+++ src/fastutils.cpp
@@ -8,21 +8,26 @@
 #include "revcomp.hpp"
 #include "interleave.hpp"
 
-void printHelp_core();
+void printHelp_core(FILE *fp);
 
 int main(int argc, char* argv[])
 {
     if(argc < 2)
     {
-        printHelp_core();
-        return EXIT_SUCCESS;
+        printHelp_core(stderr);
+        return EXIT_FAILURE;
     }
     else
     {
         string com = argv[1];
         if(com == "-h" || com == "--help")
         {
-            printHelp_core();
+            printHelp_core(stdout);
+            return EXIT_SUCCESS;
+        }
+        else if(com == "-v" || com == "--version")
+        {
+            fprintf(stdout, "%s\n", FASTUTILS_VERSION);
             return EXIT_SUCCESS;
         }
         else if(com == "stat")
@@ -59,28 +64,28 @@ int main(int argc, char* argv[])
         }
         else
         {
-            printHelp_core();
+            printHelp_core(stderr);
             return EXIT_FAILURE;
         }
     }
 }
 
-void printHelp_core()
+void printHelp_core(FILE *fp)
 {
-    fprintf(stderr, "\n");
-    fprintf(stderr, "Program: fastutils (parsing, manipulating and analysis of FASTA/FASTQ files)\n");
-    fprintf(stderr, "Version: %s\n", FASTUTILS_VERSION);
-    fprintf(stderr, "\n");
-    fprintf(stderr, "Usage: fastutils <command> [options]\n");
-    fprintf(stderr, "\n");
-    fprintf(stderr, "Commands:\n");
-    fprintf(stderr, "     stat         prints general statistics of fasta/q files\n");
-    fprintf(stderr, "     length       prints read ids and their length in tabular format\n");
-    fprintf(stderr, "     format       re-formats the fasta/q file based on user's needs\n");
-    fprintf(stderr, "     interleave   generate interleave paired end reads\n");
-    fprintf(stderr, "     revcomp      prints the reverse complement of each sequence\n");
-    fprintf(stderr, "     subsample    output a fraction of reads depending on the desired coverage\n");
-    fprintf(stderr, "     subseq       extracts a subsequence from the fasta/q file\n");
-    fprintf(stderr, "     cutN         breaks fasta entries into contigs (if containing N's)\n");
-    fprintf(stderr, "\n");
+    fprintf(fp, "\n");
+    fprintf(fp, "Program: fastutils (parsing, manipulating and analysis of FASTA/FASTQ files)\n");
+    fprintf(fp, "Version: %s\n", FASTUTILS_VERSION);
+    fprintf(fp, "\n");
+    fprintf(fp, "Usage: fastutils <command> [options]\n");
+    fprintf(fp, "\n");
+    fprintf(fp, "Commands:\n");
+    fprintf(fp, "     stat         prints general statistics of fasta/q files\n");
+    fprintf(fp, "     length       prints read ids and their length in tabular format\n");
+    fprintf(fp, "     format       re-formats the fasta/q file based on user's needs\n");
+    fprintf(fp, "     interleave   generate interleave paired end reads\n");
+    fprintf(fp, "     revcomp      prints the reverse complement of each sequence\n");
+    fprintf(fp, "     subsample    output a fraction of reads depending on the desired coverage\n");
+    fprintf(fp, "     subseq       extracts a subsequence from the fasta/q file\n");
+    fprintf(fp, "     cutN         breaks fasta entries into contigs (if containing N's)\n");
+    fprintf(fp, "\n");
 }
